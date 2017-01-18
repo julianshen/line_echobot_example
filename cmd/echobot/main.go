14d78a4a89a0acff2c71754ed111f029c6b0458c
Julian Shen <julianshen@gmail.com>
2017-01-18 17:46:35 +0800

    remove logs

--- cmd/echobot/main.go
+++ cmd/echobot/main.go
@@ -9,26 +9,21 @@ import (
 )
 
 func main() {
-	log.Println("Hello")
 	bot, e := lbotx.NewBot(
 		os.Getenv("CHANNEL_SECRET"),
 		os.Getenv("CHANNEL_TOKEN"),
 	)
 
-	log.Println("bot created")
-
 	if e != nil {
 		log.Fatal(e)
 	}
 
-	log.Println("bot OnText")
 	bot.OnText(func(context *lbotx.BotContext, text string) (bool, error) {
 		context.Messages.AddTextMessage(text)
 
 		return true, nil
 	})
 
-	log.Println("register")
 	http.Handle("/callback", bot)
 
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
