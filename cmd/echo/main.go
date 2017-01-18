b88dfef4f12da61f75fadc29882c62e83399974e
Julian Shen <julianshen@gmail.com>
2017-01-18 17:35:47 +0800

    add logs

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -14,16 +14,20 @@ func main() {
 		os.Getenv("CHANNEL_TOKEN"),
 	)
 
+	log.Println("bot created")
+
 	if e != nil {
 		log.Fatal(e)
 	}
 
+	log.Println("bot OnText")
 	bot.OnText(func(context *lbotx.BotContext, text string) (bool, error) {
 		context.Messages.AddTextMessage(text)
 
 		return true, nil
 	})
 
+	log.Println("register")
 	http.Handle("/callback", bot)
 
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
